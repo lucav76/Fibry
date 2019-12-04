5ce07d12b1f4cfaa6efa03d958d0fc7c6b425edb
Luca Venturi <l.venturi@libero.it>
2019-12-04 08:34:57 +0100

    Small improvements to remote actors

---
+++ src/main/java/eu/lucaventuri/examples/TestMessage.java
@@ -0,0 +1,30 @@
+package eu.lucaventuri.examples;
+
+public class TestMessage {
+    private String a;
+    private int b;
+
+    public TestMessage() {
+    }
+
+    public TestMessage(String a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}

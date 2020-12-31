c36194c5cafc7380948e77f79230088704680859
Luca Venturi <l.venturi@libero.it>
2020-12-31 10:19:39 +0100

    Adds the possibility to track messages after they are merged and processed

--- src/main/java/eu/lucaventuri/common/Mergeable.java
+++ src/main/java/eu/lucaventuri/common/Mergeable.java
@@ -5,4 +5,12 @@ public interface Mergeable {
     String getKey();
 
     Mergeable mergeWith(Mergeable m);
+
+    /** Tracking the execution of mergeable messages can be tricky, as they are executed in baches.
+     * This method provide an entry point, but the operation is considered optioanl. */
+    default void signalMessageProcessed() {
+        /** Suggested implementation: a list of CountDownLatches, with each latch associated to one of the initial messages;
+         * Initial messages contain a list of one latch, merged messages have more than one
+         * */
+    }
 }

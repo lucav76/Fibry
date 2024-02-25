0953d1f249ed37d1390ad3fb695f72c8c1533104
Deniz Tuerkoglu <turkoglu.deniz@gmail.com>
2024-02-25 11:06:07 +1100

    chore: bring back commented code
    Add it as an extracted method and comment out the call instead.

--- src/main/java/eu/lucaventuri/concurrent/AntiFreeze.java
+++ src/main/java/eu/lucaventuri/concurrent/AntiFreeze.java
@@ -12,12 +12,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
-/** Class that can try to un-freeze a thread stuck on waiting operations, like hanging network calls.
+/**
+ * Class that can try to un-freeze a thread stuck on waiting operations, like hanging network calls.
  * - During normal operations, the user should call notifyActivity() to prevent the activity timeout to be triggers;
  * - The user can call execute() with the logic to run.
  * - If the user does nto want to call execute(), then when task is finished, the user should call notifyFinished() to stop the scheduling of the anti-freeze
  * - At the end of the application, the user should ideally call stopScheduler(), or the scheduling thread might prevent the JVM from shutting down
- * */
+ */
 public class AntiFreeze {
     private final int activityTimeoutMs;
     private final int taskTimeoutMs;
@@ -99,17 +100,50 @@ public class AntiFreeze {
 
         try {
             System.out.println("Starting...");
+            //benchmark();
             Generator.fromProducer(yielder -> {
-                for (int i=0; i<10; i++) {
+                for (int i = 0; i < 10; i++) {
                     SystemUtils.sleep(1000);
                     yielder.yield(i);
                 }
-            }, 10,false ,2_000 ,5_000 ).toStream().forEach(System.out::println);
+            }, 10, false, 2_000, 5_000).toStream().forEach(System.out::println);
 
-            System.out.println("Finished...");
+            System.out.println("Finished 3...");
         } finally {
             System.out.println("Time passed: " + (System.currentTimeMillis() - start));
             AntiFreeze.stopScheduler();
         }
+    }
+
+    private static void benchmark() throws InterruptedException {
+        AntiFreeze frz = new AntiFreeze(1500, 7000);
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                Thread.sleep(1_000);
+                frz.notifyActivity();
+            }
+        } finally {
+            frz.notifyFinished(); // Comment to test a timeout
+        }
+
+        frz.notifyActivity();
+
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1_000);
+            frz.notifyActivity();
+        }
+
+        System.out.println("Finished 1...");
+
+        AntiFreeze.execute(1500, 7000, (frz2) -> {
+            Exceptions.rethrowRuntime(() -> {
+                for (int i = 0; i < 5; i++) {
+                    Thread.sleep(1_000);
+                    frz2.notifyActivity();
+                }
+            });
+        });
+        System.out.println("Finished 2...");
     }
 }

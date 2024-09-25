86b2ee7a9f23d0717caa6f3ae4942ceea638aff4
Luca Venturi <lventuri76@gmail.com>
2024-09-25 17:37:12 +0200

    Adds a test creating many virtual threads

--- src/main/java/eu/lucaventuri/examples/TestMassive.java
+++ src/main/java/eu/lucaventuri/examples/TestMassive.java
@@ -1,4 +1,38 @@
 package eu.lucaventuri.examples;
 
+import eu.lucaventuri.common.SystemUtils;
+import eu.lucaventuri.fibry.Stereotypes;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TestMassive {
+    public static void main(String[] args) throws InterruptedException {
+        int num = 500_000;
+        CountDownLatch latch = new CountDownLatch(num);
+        long start = System.currentTimeMillis();
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicInteger max = new AtomicInteger(0);
+
+        System.out.println("Creating " + num + " virtual threads");
+
+        for (int i=0; i<num; i++) {
+            Stereotypes.fibers().runOnce(() -> {
+                final int curCount = count.incrementAndGet();
+                int curMax = max.get();
+
+                while (curCount > curMax) {
+                    curMax = max.compareAndExchange(curMax, curCount);
+                }
+                SystemUtils.sleep(100);
+                latch.countDown();
+                count.decrementAndGet();
+            });
+        }
+
+        latch.await();
+        System.out.println("Done in " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("Max parallelism found: " + max.get());
+    }
+
 }
